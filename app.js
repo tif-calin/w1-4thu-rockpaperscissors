b45322099ae7ce62c761b0f144b5c621d59ac70c
tif-calin <culitif@tuta.io>
2021-03-25 15:45:35 -0700

    set up state and imported functions

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions and grab DOM elements
+import { getComputerThrow, doesUserWin } from './utils.js';
+
+const btnGo = document.querySelector('#button-go')
 
 // initialize state
+gameState = {
+    compThrow: getComputerThrow()
+};
+pastGames = [];
 
 // set event listeners to update state and DOM
