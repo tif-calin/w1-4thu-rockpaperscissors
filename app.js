191a26ca640ccb2f647158dd1e28e066192c1ad6
tif-calin <culitif@tuta.io>
2021-03-25 16:22:19 -0700

    implemented total game count and game stats

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const btnGo = document.querySelector('#button-go');
 const btnReset = document.querySelector('#button-reset');
 const imgCompThrow = document.querySelector('#computer-throw');
 
+const sctStats = document.querySelector('#game-stats');
+const spnTotal = document.querySelector('#stats-total');
+const spnRecord = document.querySelector('#stats-record');
+
 // initialize state
 const gameState = {
-    'compThrow': getComputerThrow()
+    'compThrow': getComputerThrow(),
+    'total': 1,
+    'wins': 0,
+    'losses': 0,
+    'draws': 0
 };
-const pastGames = [];
 
 // set event listeners to update state and DOM
 btnReset.addEventListener('click', () => {
@@ -23,12 +30,32 @@ btnReset.addEventListener('click', () => {
     // show or hide relevent buttons
     btnGo.disabled = false;
     btnReset.classList.add('hidden');
+
+    // show stats
+    gameState['total']++;
+    spnTotal.textContent = gameState['total'];
+    sctStats.classList.remove('hidden');
 });
 
 btnGo.addEventListener('click', () => {
     // reveal computer's throw
     imgCompThrow.src = './assets/' + gameState['compThrow'] + '.png';
     imgCompThrow.alt = gameState['compThrow'] + ' emoji';
+
+    // win or lose?
+    const userThrow = document.querySelector('input:checked').value;
+    const result = doesUserWin(userThrow, gameState['compThrow']);
+    switch(result) {
+        case 'win':
+            gameState['wins']++;
+            break;
+        case 'lose':
+            gameState['losses']++;
+            break;
+        default:
+            gameState['draws']++;
+    }
+    spnRecord.textContent = `(${gameState['wins']}-${gameState['draws']}-${gameState['losses']})`;
 
     // show and disable relevent buttons
     btnGo.disabled = true;
